aa9baa2c6c82c1f18621ef1bf61bef7747c87f15
nikitadyukov <nikita.n1kolaevich@yandex.ru>
2021-09-17 13:17:11 +0300

    добавлен фон

--- Example2/ViewController.swift
+++ Example2/ViewController.swift
@@ -8,11 +8,17 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var label = UILabel()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         view.backgroundColor = .blue
+        label.drawText(in: CGRect(x: view.center.x, y: view.center.y, width: 100, height: 50))
+        label.text = "Exampleee"
+        label.textColor = .black
+        view.addSubview(label)
     }
     
 
